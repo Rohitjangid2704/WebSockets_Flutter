326dcb513ab7f9a720d40f125aa1eb6682aca461
Rohit jangid <rohitjangid2704@gmail.com>
2024-08-08 14:16:51 +0530

    Update websocket_service.dart
    not used

--- lib/websocket_service.dart
+++ lib/websocket_service.dart
@@ -1,17 +1,1 @@
-// import 'package:web_socket_channel/web_socket_channel.dart';
-//
-// class WebSocketService {
-//   final WebSocketChannel channel;
-//
-//   WebSocketService(String url) : channel = WebSocketChannel.connect(Uri.parse(url));
-//
-//   Stream<dynamic> get stream => channel.stream;
-//
-//   void send(String message) {
-//     channel.sink.add(message);
-//   }
-//
-//   void dispose() {
-//     channel.sink.close();
-//   }
-// }
+
